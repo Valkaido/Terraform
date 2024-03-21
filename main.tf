ed847c9970f6d05eefc10d6346d3b0c2483d6111
Valkaido <valentin.61@live.fr>
2024-03-21 15:57:53 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("${path.module}/id_rsa.pub")  # Corrected path to SSH public key
+    public_key = file("/home/azureuser/.ssh/id_rsa.pub")
   }
 
   os_disk {
